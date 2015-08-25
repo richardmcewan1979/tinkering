a106384e3417840091da041c740cfdf5bb6d9089
richardmcewan1979 <richmcewan@icloud.com>
2015-08-25 18:38:15 +0100

    Create afunction.py
    Added a comment

---
+++ afunction.py
@@ -0,0 +1,11 @@
+#simple function py3
+
+def square(x):
+    '''this square a variable'''
+    y=x*x
+    return y
+
+x = int(input('Give me a number to square: '))
+print(square(x))
+
+#py2: raw_input('Give me a number to square: ')
